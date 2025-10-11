1b1af09c9fbea77b26a458f7a2fe8a28a4834070
Hyder Ali <haiderbrohi222@gmail.com>
2025-10-11 15:48:00 +0500

    ProfileScreen code

--- app/src/main/java/com/example/komatoapp/presentation/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/komatoapp/presentation/screens/ProfileScreen.kt
@@ -1,5 +1,6 @@
 package com.example.komatoapp.presentation.screens
 
+import android.provider.ContactsContract
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,12 +13,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -25,9 +28,16 @@ import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateSetOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -188,11 +198,17 @@ fun ZomatoGold() {
 }
 
 @Composable
-fun ColumnGrid(card: cardItem.Column.Grid) {
-    Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp, vertical = 8.dp),
-        verticalAlignment = Alignment.CenterVertically){
+fun ColumnGrid(card: cardItem.ColoumnGrid) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Card(
-            modifier = Modifier.fillMaxWidth().weight(1f),
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
             colors = CardDefaults.cardColors(containerColor = Color.White)
         ) {
             Column(modifier = Modifier.fillMaxSize()) {
@@ -200,16 +216,19 @@ fun ColumnGrid(card: cardItem.Column.Grid) {
                     painterResource(R.drawable.outline_bookmark_24),
                     contentDescription = "Collection",
                     tint = Color.Gray,
-                    modifier = Modifier.padding(start = 20.dp, top = 15.dp, bottom = 5.dp)
+                    modifier = Modifier
+                        .padding(start = 20.dp, top = 15.dp, bottom = 5.dp)
                         .size(30.dp)
                 )
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp, vertical = 10.dp)
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 10.dp, vertical = 10.dp)
                 ) {
                     Text(
                         text = "Collection",
-                        color =Color.Black,
+                        color = Color.Black,
                         fontSize = 15.sp,
                         fontWeight = FontWeight.SemiBold
                     )
@@ -218,7 +237,9 @@ fun ColumnGrid(card: cardItem.Column.Grid) {
         }
         Spacer(modifier = Modifier.width(10.dp))
         Card(
-            modifier = Modifier.fillMaxWidth().weight(1f),
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
             colors = CardDefaults.cardColors(containerColor = Color.White)
         ) {
             Column(modifier = Modifier.fillMaxSize()) {
@@ -226,13 +247,17 @@ fun ColumnGrid(card: cardItem.Column.Grid) {
                     painterResource(R.drawable.rupeesymbol),
                     contentDescription = "Money",
                     tint = Color.Gray,
-                    modifier =  Modifier.padding(
-                        start = 20.dp,top = 15.dp, bottom = 5.dp
-                    ).size(30.dp)
+                    modifier = Modifier
+                        .padding(
+                            start = 20.dp, top = 15.dp, bottom = 5.dp
+                        )
+                        .size(30.dp)
                 )
                 Row(
-                    verticalAlignment =  Alignment.CenterVertically,
-                    modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp, vertical = 10.dp)
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 10.dp, vertical = 10.dp)
                 ) {
                     Text(
                         text = "Money",
@@ -242,13 +267,13 @@ fun ColumnGrid(card: cardItem.Column.Grid) {
                     )
 
                     Spacer(modifier = Modifier.width(10.dp))
-                    Row (
+                    Row(
                         modifier = Modifier
                             .size(width = 30.dp, height = 20.dp)
                             .background(color = colorResource(R.color.purple_500)),
                         horizontalArrangement = Arrangement.Center,
                         verticalAlignment = Alignment.CenterVertically
-                    ){
+                    ) {
                         Text(
                             text = "Rs0",
                             color = colorResource(R.color.purple_500),
@@ -262,6 +287,205 @@ fun ColumnGrid(card: cardItem.Column.Grid) {
 }
 
 @Composable
-fun Lazycolumn() {
+fun Lazycoloumm() {
+    val cardList = listOf(
+        cardItem.ColoumnGrid(name = "Ayush"),
+        cardItem.FirstCard(Profilename = "Your Profile", percentagetext = "48%Completed"),
+        cardItem.SecondCard(vegtext = "Veg Mode"),
+        cardItem.ThirdCard(Appearance = "Appearance"),
+        cardItem.FourthCard(rating = "Your rating"),
+        cardItem.FifthCard(Food_Order = "Food Orders"),
+        cardItem.SixthCard(Titilename = "Dining and Experience"),
+        cardItem.SeventhCard(title = "done"),
+        cardItem.EighthCard(Title = "done"),
+        cardItem.NinthCard(Title = "done"),
+        cardItem.TenCard(Title = "done"),
+        cardItem.EleventhCard(Title = "eleventhdone")
+    )
 
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(cardList) { item ->
+
+            when (item) {
+                is cardItem.ColoumnGrid -> ColumnGrid(item)
+                is cardItem.FirstCard -> ProductCard(item)
+                is cardItem.SecondCard ->
+            }
+        }
+    }
+}
+
+@Composable
+fun ProductCard(card: cardItem.FirstCard) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 6.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp, vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Icon(
+                imageVector = Icons.Default.AccountCircle,
+                contentDescription = "Your Profile Icon",
+                modifier = Modifier.size(25.dp),
+                tint = Color.LightGray
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = "Your Profile", color = Color.Black)
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = "48% Completed",
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(12.dp))
+                    .background(color = colorResource(R.color.purple_500)),
+                color = colorResource(R.color.purple_500),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 15.sp
+            )
+
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                contentDescription = "Forward arrow",
+                modifier = Modifier.padding(start = 5.dp),
+                tint = Color.DarkGray
+            )
+        }
+    }
+}
+
+@Composable
+fun VegModeCard(card: cardItem.SecondCard) {
+    var switch1 by remember { mutableStateOf(false) }
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 6.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    )
+    {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp, vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painterResource(R.drawable.veg_icon),
+                contentDescription = "Veg Mode Icon",
+                modifier = Modifier.size(22.dp),
+                tint = colorResource(R.color.purple_500)
+            )
+
+            Spacer(modifier = Modifier.width(6.dp))
+            Text(text = "Veg Mode", color = Color.Black)
+            Spacer(modifier = Modifier.weight(1f))
+            //Toggle Button for the veg mode
+            Switch(
+                checked = switch1,
+                onCheckedChange = { switch1 = it },
+                colors = SwitchDefaults.colors(checkedIconColor = Color.Green),
+                modifier = Modifier.height(16.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun AppearanceCard(card: cardItem.ThirdCard) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 6.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    )
+    {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp, vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Appearance one icon need to bee added download
+            Icon(
+                painterResource(R.drawable.paint),
+                contentDescription = "Your Profile",
+                modifier = Modifier.size(20.dp),
+                tint = Color.LightGray
+            )
+            Text(text = "Appearance", color = Color.Black)
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = "LIGHT",
+                fontWeight = FontWeight.Bold,
+                color = Color.Gray
+            )
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                contentDescription = "Forward arrow",
+                tint = Color.Gray
+            )
+        }
+    }
+}
+
+@Composable
+fun RatingCard(card: cardItem.FourthCard) {
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 6.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    )
+    {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp, vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // rating icon need to be added download
+            Icon(painter = painterResource(R.drawable.rating),
+                contentDescription = "Rating icon",
+                modifier = Modifier.size(25.dp),
+                tint = Color.LightGray)
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = "Your rating",color = Color.Black)
+            Spacer(modifier = Modifier.weight(1f))
+            Icon(painterResource(R.drawable.startrating),
+                contentDescription = "Star rating",
+                tint = Color.Unspecified)
+
+        }
+
+
+    }
+}
+
+@Composable
+fun FoodOrderCard(card: cardItem.FifthCard) {
+
+}
+
+
+sealed class cardItem {
+
+    data class ColoumnGrid(val name: String) : cardItem()
+    data class FirstCard(val Profilename: String, val percentagetext: String) : cardItem()
+    data class SecondCard(val vegtext: String) : cardItem()
+    data class ThirdCard(val Appearance: String) : cardItem()
+    data class FourthCard(val rating: String) : cardItem()
+    data class FifthCard(val Food_Order: String) : cardItem()
+    data class SixthCard(val Titilename: String) : cardItem()
+    data class SeventhCard(val title: String) : cardItem()
+    data class EighthCard(val Title: String) : cardItem()
+    data class NinthCard(val Title: String) : cardItem()
+    data class TenCard(val Title: String) : cardItem()
+    data class EleventhCard(val Title: String) : cardItem()
 }
