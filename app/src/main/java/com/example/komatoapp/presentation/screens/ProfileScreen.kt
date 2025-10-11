b87b50b320b94e702a6c9002888e62ae888ede57
Hyder Ali <haiderbrohi222@gmail.com>
2025-10-11 14:37:22 +0500

    ProfileScreen code

--- app/src/main/java/com/example/komatoapp/presentation/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/komatoapp/presentation/screens/ProfileScreen.kt
@@ -1,9 +1,267 @@
 package com.example.komatoapp.presentation.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.komatoapp.R
 
 @Composable
 fun ProfileScreen(navController: NavHostController) {
+
+    Scaffold(
+        modifier = Modifier.fillMaxWidth(),
+        topBar = {
+            TopAppBar(
+                modifier = Modifier.background(color = Color.White),
+                title = {},
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "ArrowBack"
+                        )
+                    }
+                })
+
+        }) { values ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.Gray)
+                .padding(values)
+        ) {
+
+            Spacer(modifier = Modifier.height(8.dp))
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(110.dp)
+                    .padding(horizontal = 10.dp),
+                shape = RoundedCornerShape(
+                    topStart = 12.dp,
+                    topEnd = 12.dp,
+                    bottomStart = 0.dp,
+                    bottomEnd = 0.dp
+                ),
+                colors = CardDefaults.cardColors(Color.White)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp, vertical = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    Icon(
+                        modifier = Modifier
+                            .size(80.dp)
+                            .clip(CircleShape),
+                        painter = painterResource(id = R.drawable.profielogo),
+                        contentDescription = "Profile Image",
+                        tint = Color.Unspecified
+                    )
+                    Spacer(modifier = Modifier.width(7.dp))
+                    Column {
+                        Text(
+                            text = "Name",
+                            color = Color.Black,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 23.sp
+                        )
+                        Text(
+                            text = "gmail2025gmail.com",
+                            color = Color.DarkGray,
+                            fontSize = 12.sp
+                        )
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(2.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = "View activity",
+                                fontSize = 13.sp,
+                                color = colorResource(id = R.color.purple_500)
+                            )
+
+                            Icon(
+                                imageVector = Icons.Default.PlayArrow,
+                                tint = Color.DarkGray,
+                                contentDescription = "Forward Arrow",
+                                modifier = Modifier.size(16.dp)
+                            )
+                        }
+                    }
+                }
+
+            }
+            // Here will be the lazy column
+            ZomatoGold()
+            // column_grid()
+            Lazycolumn()
+        }
+    }
+}
+
+@Composable
+fun ZomatoGold() {
+
+    Card(
+        colors = CardDefaults.cardColors(containerColor = Color.Black),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp)
+            .height(65.dp),
+        shape = RoundedCornerShape(
+            bottomEnd = 12.dp,
+            bottomStart = 12.dp,
+            topEnd = 0.dp,
+            topStart = 0.dp
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp, vertical = 14.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Icon(
+                painterResource(R.drawable.goldicon1),
+                contentDescription = "Zomato gold Image",
+                tint = Color.Unspecified
+            )
+
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                text = "Join Zomato Gold",
+                fontWeight = FontWeight.Bold,
+                color = colorResource(R.color.purple_500)
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                contentDescription = "Arrow Right",
+                tint = Color.White
+            )
+        }
+    }
+}
+
+@Composable
+fun ColumnGrid(card: cardItem.Column.Grid) {
+    Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically){
+        Card(
+            modifier = Modifier.fillMaxWidth().weight(1f),
+            colors = CardDefaults.cardColors(containerColor = Color.White)
+        ) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                Icon(
+                    painterResource(R.drawable.outline_bookmark_24),
+                    contentDescription = "Collection",
+                    tint = Color.Gray,
+                    modifier = Modifier.padding(start = 20.dp, top = 15.dp, bottom = 5.dp)
+                        .size(30.dp)
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp, vertical = 10.dp)
+                ) {
+                    Text(
+                        text = "Collection",
+                        color =Color.Black,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.width(10.dp))
+        Card(
+            modifier = Modifier.fillMaxWidth().weight(1f),
+            colors = CardDefaults.cardColors(containerColor = Color.White)
+        ) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                Icon(
+                    painterResource(R.drawable.rupeesymbol),
+                    contentDescription = "Money",
+                    tint = Color.Gray,
+                    modifier =  Modifier.padding(
+                        start = 20.dp,top = 15.dp, bottom = 5.dp
+                    ).size(30.dp)
+                )
+                Row(
+                    verticalAlignment =  Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp, vertical = 10.dp)
+                ) {
+                    Text(
+                        text = "Money",
+                        color = Color.Black,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Row (
+                        modifier = Modifier
+                            .size(width = 30.dp, height = 20.dp)
+                            .background(color = colorResource(R.color.purple_500)),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        Text(
+                            text = "Rs0",
+                            color = colorResource(R.color.purple_500),
+                            fontSize = 14.sp
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Lazycolumn() {
 
 }
