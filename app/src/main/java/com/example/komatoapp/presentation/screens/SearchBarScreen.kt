f771119ffea25ea13a5ae4dd193be3865b38b8aa
Hyder Ali <haiderbrohi222@gmail.com>
2025-10-17 22:28:22 +0500

    SearchBarScreen code

--- app/src/main/java/com/example/komatoapp/presentation/screens/SearchBarScreen.kt
+++ app/src/main/java/com/example/komatoapp/presentation/screens/SearchBarScreen.kt
@@ -1,5 +1,6 @@
 package com.example.komatoapp.presentation.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -17,6 +18,7 @@ import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Icon
@@ -35,6 +37,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.stylusHoverIcon
@@ -44,6 +47,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -195,7 +199,9 @@ fun FoodCategoryList(modifier: Modifier = Modifier) {
             letterSpacing = 2.sp,
             fontFamily = FontFamily.SansSerif
         ),
-        modifier = Modifier.padding(vertical = 8.dp).padding(start =15.dp),
+        modifier = Modifier
+            .padding(vertical = 8.dp)
+            .padding(start = 15.dp),
         textAlign = TextAlign.Center
     )
     val foodCategories = listOf(
@@ -230,7 +236,8 @@ fun FoodCategoryList(modifier: Modifier = Modifier) {
 
     LazyVerticalGrid(
         columns = GridCells.Fixed(3) ,// Display 3 items per row
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
             .padding(vertical = 8.dp),
         contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
@@ -244,5 +251,39 @@ fun FoodCategoryList(modifier: Modifier = Modifier) {
                 onClick= {selectedCategoryIndex = index}
             )
         }
+    }
+}
+
+@Composable
+fun FoodCategoryItem(
+    category : FoodCategory,
+    isSelected : Boolean,
+    onClick :() -> Unit
+) {
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.width(100.dp)
+    ) {
+        Image(
+            painter = painterResource(id = category.image),
+            contentDescription = category.name,
+            modifier = Modifier
+                .size(80.dp)
+                .clip(CircleShape)
+        )
+        Spacer(Modifier.height(8.dp))
+
+        Text(
+            text = category.name,
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                color  = Color.DarkGray,
+                textAlign = TextAlign.Center
+            ),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
     }
 }
