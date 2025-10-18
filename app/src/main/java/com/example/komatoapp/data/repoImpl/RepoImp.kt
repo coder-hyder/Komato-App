d52fd6fcd138d6fd8d5456091f823dbdcb8f1d91
Hyder Ali <haiderbrohi222@gmail.com>
2025-10-18 08:41:29 +0500

    Repository implementation

---
+++ app/src/main/java/com/example/komatoapp/data/repoImpl/RepoImp.kt
@@ -0,0 +1,62 @@
+package com.example.komatoapp.data.repoImpl
+
+import com.example.komatoapp.common.ResultState
+import com.example.komatoapp.common.USER_COLLECTION
+import com.example.komatoapp.domain.models.UserData
+import com.example.komatoapp.domain.repo.Repo
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+
+class RepoImp(val firebaseAuth: FirebaseAuth, val firestore: FirebaseFirestore) : Repo {
+
+    override fun registerWithEmailAndPassword(userData: UserData): Flow<ResultState<String>> =
+        callbackFlow {
+            trySend(ResultState.Loading)
+
+            firebaseAuth.createUserWithEmailAndPassword(userData.email,userData.password)
+                .addOnCompleteListener {
+                    if (it.isSuccessful){
+                        firestore.collection(USER_COLLECTION)
+                            .document(it.result?.user?.uid.toString()).set(userData)
+                            .addOnCompleteListener {
+                                if (it.isSuccessful){
+                                    trySend(ResultState.Success("User Registered Successfully"))
+                                }else{
+                                    if (it.exception !=null){
+                                        trySend(ResultState.Error(it.exception?.localizedMessage.toString()))
+                                    }
+
+                                }
+                            }
+                        trySend(ResultState.Success("User Registered Successfully"))
+
+                    }else{
+                        if (it.exception !=null){
+                            trySend(ResultState.Error(it.exception?.localizedMessage.toString()))
+                        }
+                    }
+                }
+
+            awaitClose { close() }
+
+        }
+    override fun loginWithEmailAndPassword(userData: UserData): Flow<ResultState<String>>  = callbackFlow{
+
+            trySend(ResultState.Loading)
+
+        firebaseAuth.signInWithEmailAndPassword(userData.email, userData.password)
+            .addOnCompleteListener {
+                if(it.isSuccessful){
+                    trySend(ResultState.Success("User Logged In Successfully"))
+                }else{
+                    if (it.exception !=null){
+                        trySend(ResultState.Error(it.exception?.localizedMessage.toString()))
+                    }
+                }
+            }
+        awaitClose { close() }
+    }
+}
