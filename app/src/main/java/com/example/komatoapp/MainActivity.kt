890ad809d388cef256cbd8ccd4b0bb1847e22375
Hyder Ali <haiderbrohi222@gmail.com>
2025-10-20 08:36:22 +0500

    MainActivity scroll code according ui

--- app/src/main/java/com/example/komatoapp/MainActivity.kt
+++ app/src/main/java/com/example/komatoapp/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
@@ -25,18 +26,24 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.komatoapp.presentation.navigation.AppNavigation
 import com.example.komatoapp.ui.theme.KomatoAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -44,67 +51,40 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
+
+            // Track scroll direction
+            var isVisible by remember { mutableStateOf(true) }
+            var lastScrollOffset by remember { mutableIntStateOf(0) }
+            var lastIndex by remember { mutableIntStateOf(0) }
+            val listState = rememberLazyListState()
+
+            // Listen to scroll events
+            LaunchedEffect(listState) {
+                snapshotFlow { listState.firstVisibleItemIndex to listState.firstVisibleItemScrollOffset }
+                    .distinctUntilChanged()
+                    .collect { (index, scrollOffset) ->
+                        if (index > lastIndex || (index == lastIndex && scrollOffset > lastScrollOffset + 50)) {
+                            // Scrolling Down -> Hide BottomNavBar
+                            isVisible = false
+                        } else if (index < lastIndex || (scrollOffset < lastScrollOffset - 50)) {
+                            // Scrolling Up -> Show BottomNavBar
+                            isVisible = true
+                        }
+                        lastIndex = index
+                        lastScrollOffset = scrollOffset
+                    }
+            }
+
+
             KomatoAppTheme {
-                Box(modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center){
-                    BottomSheet()
-                }
+                AppNavigation(
+                    isVisible = isVisible,
+                    listState = listState
+                )
             }
         }
     }
 }
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun BottomSheet(modifier: Modifier = Modifier,) {
-    val sheetState = rememberModalBottomSheetState()
-    val scope = rememberCoroutineScope()
-    var showBottomSheet by remember { mutableStateOf(false) }
-
-    Button(onClick = {showBottomSheet = true}) {
-        Text(text = "Show BottomSheet ")
-    }
-
-    if (showBottomSheet){
-        ModalBottomSheet(onDismissRequest = {
-            showBottomSheet = false
-        },
-            sheetState = sheetState ) {
-            Button(onClick = {
-                scope.launch{sheetState.hide()}.invokeOnCompletion{
-                    if (!sheetState.isVisible){
-                        showBottomSheet = false
-                    }
-                }
-            })
-            { Text("Hide Button")}
-
-            var text by remember { mutableStateOf("") }
-            OutlinedTextField(
-                value = text,
-                onValueChange = { text = it },
-                modifier = Modifier.padding(horizontal = 16.dp),
-                label = { Text("Text field") }
-            )
-            LazyColumn {
-                items(25) {
-                    ListItem(
-                        headlineContent = { Text("Item $it") },
-                        leadingContent = {
-                            Icon(
-                                Icons.Default.Favorite,
-                                contentDescription = "Localized description"
-                            )
-                        },
-                        colors =
-                            ListItemDefaults.colors(
-                                containerColor = MaterialTheme.colorScheme.surfaceContainerLow
-                            ),
-                    )
-                }
-            }
-        }
-        }
-    }
